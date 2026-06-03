70ec3b7abf193ea840b08272afbb105b6544c5ed
Songmx <106227069+Willacold@users.noreply.github.com>
2026-06-03 22:11:22 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,9 +4,9 @@ window.onload = function() {
     const carousels = [
         ['panorama-showcase-carousel', typeof forward_backward_items !== 'undefined' ? forward_backward_items : [], typeof forward_backward_template !== 'undefined' ? forward_backward_template : null],
         ['orbit-compare-carousel', typeof orbit_compare_items !== 'undefined' ? orbit_compare_items : [], typeof orbit_compare_template !== 'undefined' ? orbit_compare_template : null],
-        ['outdoor-carousel', typeof outdoor_items !== 'undefined' ? outdoor_items : [], typeof carousel_item_template !== 'undefined' ? carousel_item_template : null],
-        ['indoor-carousel', typeof indoor_items !== 'undefined' ? indoor_items : [], typeof carousel_item_template !== 'undefined' ? carousel_item_template : null],
-        ['details-carousel', typeof details_items !== 'undefined' ? details_items : [], typeof carousel_item_template !== 'undefined' ? carousel_item_template : null],
+        ['compare-perspective-carousel', typeof compare_perspective_items !== 'undefined' ? compare_perspective_items : [], typeof compare_column_template !== 'undefined' ? compare_column_template : null],
+        ['compare-panoramic-carousel', typeof compare_panoramic_items !== 'undefined' ? compare_panoramic_items : [], typeof compare_column_template !== 'undefined' ? compare_column_template : null],
+        ['compare-fisheye-carousel', typeof compare_fisheye_items !== 'undefined' ? compare_fisheye_items : [], typeof compare_column_template !== 'undefined' ? compare_column_template : null],
         ['omnirooms-scenes-carousel', typeof omnirooms_scenes_items !== 'undefined' ? omnirooms_scenes_items : [], typeof omnirooms_scene_template !== 'undefined' ? omnirooms_scene_template : null],
     ];
 
@@ -15,4 +15,8 @@ window.onload = function() {
             make_carousel(id, template, items, 1, 1);
         }
     });
+
+    if (typeof init_qual_eval_compare_layout === 'function') {
+        init_qual_eval_compare_layout();
+    }
 };
